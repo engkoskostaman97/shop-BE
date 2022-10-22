ad5257c883302f753ded2973b8f9d045d4934e27
engkos <engkoskostaman759@gmail.com>
2022-10-23 01:35:19 +0700

    dev

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -11,14 +11,15 @@ import (
 )
 
 var DB *gorm.DB
-var DB_HOST = os.Getenv("DB_HOST")
-var DB_USER = os.Getenv("DB_USER")
-var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-var DB_NAME = os.Getenv("DB_NAME")
-var DB_PORT = os.Getenv("DB_PORT")
 
 func DatabaseInit() {
 	var err error
+	// Get `host`, `user`, `password`, `database name`, and `port` from env ...
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 	// dsn := "root:@tcp(127.0.0.1:3306)/dumbmerch?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
